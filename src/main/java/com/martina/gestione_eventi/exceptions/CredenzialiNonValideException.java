81ad4ab1e284b732d68395f90c51aba11ad4f91d
Martina <acetomartina141@gmail.com>
2026-07-17 15:20:24 +0200

    Implementato servizio di autenticazione utenti

---
+++ src/main/java/com/martina/gestione_eventi/exceptions/CredenzialiNonValideException.java
@@ -0,0 +1,8 @@
+package com.martina.gestione_eventi.exceptions;
+
+public class CredenzialiNonValideException extends RuntimeException {
+
+    public CredenzialiNonValideException(String message) {
+        super(message);
+    }
+}
